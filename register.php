49080454de0acc92a9a78c35af901e9d0a918137
Adam Clarke <adamclarke0905@gmail.com>
2024-10-14 10:28:03 +0100

    created form

--- register.php
+++ register.php
@@ -24,3 +24,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 ?>
 
+<form method="POST">
+    <input type="text" name="first_name" placeholder="First Name" required>
+    <input type="text" name="last_name" placeholder="Last Name" required>
+    <input type="email" name="email" placeholder="Email" required>
+    <input type="password" name="password" placeholder="Password" required>
+    <button type="submit">Register</button>
+</form>
