2681e74d1d3e58fbb7a3d3b7373bec59ef7aff63
Adam Clarke <adamclarke0905@gmail.com>
2024-10-14 12:17:42 +0100

    products page now requires log in

--- products.php
+++ products.php
@@ -1,1 +1,22 @@
 <?php include 'includes/nav_general.php'; ?>
+
+<?php
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    header('Location: login.php'); // Redirect to login page if not logged in
+    exit;
+}
+
+// If user is logged in, display products
+include 'connect_db.php';
+
+$sql = "SELECT * FROM products";
+$result = $conn->query($sql);
+
+echo "<h1>Products</h1>";
+while ($product = $result->fetch_assoc()) {
+    echo "<p>" . $product['name'] . " - $" . $product['price'] . "</p>";
+}
+?>
+
+<a href="logout.php">Logout</a>
