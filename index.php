a874f7ac02d696fa9e339ec3c9b7f835754b09f8
Adam Clarke <adamclarke0905@gmail.com>
2024-10-14 12:15:48 +0100

    added placeholder homepage

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<?php include 'includes/nav_general.php'; ?>
+<?php
+session_start();
+include 'includes/nav_general.php';
+
+echo "<h1>Welcome to MKTIME!</h1>";
+echo "<p>Discover the best products!</p>";
+
+if (isset($_SESSION['user_id'])) {
+    echo "<a href='products.php'>View Products</a>";
+} else {
+    echo "<a href='register.php'>Register</a> or <a href='login.php'>Login</a>";
+}
+
+include 'includes/footer.php'; // Include footer
+?>
