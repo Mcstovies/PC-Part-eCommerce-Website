cdcd51498eba9d48409024e2141869d457481662
Adam Clarke <adamclarke0905@gmail.com>
2024-10-14 10:27:10 +0100

    insert user in user database on mysql and redirects to login page after successful registration

--- register.php
+++ register.php
@@ -8,5 +8,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'];
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT); // Hash the password for security
     $reg_date = date('Y-m-d H:i:s'); // Get the current data and time for registration
+
+    // Insert user into the database
+    $sql = "INSERT INTO users (first_name, last_name, email, pass, reg_date) VALUES (?, ?, ?, ?, ?)";
+    $stmt = $conn ->prepare($sql);
+    $stmt->bind_param('sssss', $first_name, $last_name, $email, $password, $reg_date);
+
+    if ($stmt->execute()) {
+        echo "Registration successful!";
+        header('Location: login.php'); // Redirect to login page after successful registration
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+    $stmt->close();
 }
 ?>
+
