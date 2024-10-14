64fb0ec50e62eb273f6afa064b51b7eb1d162128
Adam Clarke <adamclarke0905@gmail.com>
2024-10-14 12:10:36 +0100

    created navbar php for front-end pages

---
+++ includes/nav_general.php
@@ -0,0 +1,34 @@
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="register.php">Register</a></li>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="products.php">Products</a></li>
+    </ul>
+</nav>
+
+<style>
+    nav {
+        background-color: #333;
+        overflow: hidden;
+    }
+    nav ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        display: flex;
+    }
+    nav ul li {
+        float: left;
+    }
+    nav ul li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+    }
+    nav ul li a:hover {
+        background-color: #575757;
+    }
+</style>
