b4d1301c19f88c01dcd200cbf11c6566dc7ebf39
Adam Clarke <adamclarke0905@gmail.com>
2024-10-15 10:49:15 +0100

    added bootstrap cards to products page and it is now showing items descirption and image

--- products.php
+++ products.php
@@ -13,10 +13,26 @@ include 'connect_db.php';
 $sql = "SELECT * FROM products";
 $result = $link->query($sql);
 
-echo "<h1>Products</h1>";
+echo "<div class='container mt-5'>";
+echo "<h1 class='text-center'>Products</h1>";
+echo "<div class='row'>"; // Bootstrap row to hold the product cards
+
 while ($product = $result->fetch_assoc()) {
-    echo "<p>" . $product['name'] . " - $" . $product['price'] . "</p>";
+    // Each product will be in its own Bootstrap column with a card layout
+    echo "<div class='col-md-4 mb-4'>"; // Bootstrap column
+    echo "<div class='card' style='width: 18rem;'>"; // Card start
+    echo "<img src='" . $product['item_img'] . "' class='card-img-top' alt='" . $product['item_name'] . "'>"; // Display the image
+    echo "<div class='card-body'>"; // Card body
+    echo "<h5 class='card-title'>" . $product['item_name'] . "</h5>"; // Display the product name
+    echo "<p class='card-text'>" . $product['item_desc'] . "</p>"; // Display the product description
+    echo "<p class='card-text'><strong>$" . $product['item_price'] . "</strong></p>"; // Display the price
+    echo "</div>"; // End of card body
+    echo "</div>"; // End of card
+    echo "</div>"; // End of Bootstrap column
 }
+
+echo "</div>"; // End of row
+echo "</div>"; // End of container
 ?>
 
-<a href="logout.php">Logout</a>
+<a href="logout.php" class="btn btn-danger mt-3">Logout</a>
