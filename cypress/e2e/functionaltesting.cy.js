648f2b1b42b2f2490a4dde2d55fa460df683af3d
Adam Clarke <adamclarke0905@gmail.com>
2024-10-27 20:43:18 +0000

    added log in and product access test

--- cypress/e2e/functionaltesting.cy.js
+++ cypress/e2e/functionaltesting.cy.js
@@ -1,5 +1,12 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('Login Access Control', () => {
+  it('should redirect to login if user is not logged in when accessing products page', () => {
+      cy.visit('http://localhost/codespaceproject/products.php'); // Try to access products page
+      cy.url().should('include', '/login.php'); // Verify redirected to login
+      cy.get('form').within(() => {
+          cy.get('input[name="email"]').type('123@gmail.com'); // Enter email
+          cy.get('input[name="password"]').type('123'); // Enter password
+          cy.get('button[type="submit"]').click(); // Submit login form
+      });
+      cy.url().should('include', '/products.php'); // Verify now on products page
+  });
+});
