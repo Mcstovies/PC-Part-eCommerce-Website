2ac11664007426c140f7d8b648f0b152da807f35
Adam Clarke <adamclarke0905@gmail.com>
2024-10-28 12:08:29 +0000

    testing log in and adding a product to the cart

--- cypress/e2e/functionaltesting.cy.js
+++ cypress/e2e/functionaltesting.cy.js
@@ -10,3 +10,22 @@ describe('Login Access Control', () => {
       cy.url().should('include', '/products.php'); // Verify now on products page
   });
 });
+
+describe('Add Product to Cart', () => {
+  it('should add a product to the cart and show success message', () => {
+      cy.visit('http://localhost/codespaceproject/products.php'); // Navigate to products page
+      cy.url().should('include', '/login.php'); // Verify redirected to login
+      cy.get('form').within(() => {
+          cy.get('input[name="email"]').type('123@gmail.com'); // Enter email
+          cy.get('input[name="password"]').type('123'); // Enter password
+          cy.get('button[type="submit"]').click(); // Submit login form
+      });
+      cy.url().should('include', '/products.php'); // Verify now on products page
+      cy.get('.card').first().within(() => {
+          cy.get('button').contains('Add to Cart').click(); // Click on the first product's 'Add to Cart' button
+      });
+      cy.get('.alert').should('contain', 'Product added to cart!'); // Check success message
+      cy.get('a').contains('View Cart').click(); // Navigate to cart page
+      cy.get('.cart-item').should('have.length.greaterThan', 0); // Verify cart has at least 1 item
+  });
+});
